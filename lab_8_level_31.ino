4850900ec5847c353ee5b91a8bf2ae71ec8d2c7a
Bailz09 <122489154+Bailz09@users.noreply.github.com>
2023-03-15 14:38:59 -0500

    Add files via upload

--- lab_8_level_31.ino
+++ lab_8_level_31.ino
@@ -1,27 +1,26 @@
-// C++ code
-//
+// Kevin Bailey (200544764@student.georgianc.on.ca)
+// Tuesday March 14 2023
+// Lab 8 Level 3 LCD choices
 
+//declare variables
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(12,11,5,4,3,2);
-
-//making a string array
-
 int buttonPin = 6;
 String cars[]={"Ferrari", "Ford", "GM", "Maserati", "Cadillac"};
 String boats[]={"Skater", "Talon", "Hydrostream", "Martini", "Cigarette"};
 String toys[]={"Skidoo", "Seadoo", "SidexSide", "Dirtbike", "Motorcycle"};
 String choice[3]={};
-int count=0;
-void setup()
-{
-Serial.begin(9600);
-lcd.begin (16,2); //Initialize the LCD.
-pinMode(buttonPin, INPUT);
-}
+int count=-1;
 
-void loop()
+void setup()//setup function runs once
+{
+Serial.begin(9600);//Serial monitor
+lcd.begin (16,2); //Initialize the LCD.
+pinMode(buttonPin, INPUT);//setup buttonPin as an INPUT pin 
+}	  
+void loop()//The loop function runs forever
 { 
+  start(); //functions
   car();
   boat(); 
   toy();
@@ -29,23 +28,22 @@ void loop()
 }
 void car()
 {
-  while(count==0)
+  while(count==0)//while count variable is zero print the cars array
   {
-    for(int i=0; i<5; i++)
+    for(int i=0; i<5; i++)//for loop to cycle through the items in the array
     {
       lcd.clear();
 	  lcd.setCursor(0,0);
 	  lcd.print(cars[i]);
   	  delay(750);
-    
-	  if (digitalRead(buttonPin)==HIGH)
+	  if (digitalRead(buttonPin)==HIGH)//checks if the button is pressed
 	  {
-        lcd.clear();
-        count++;
-  	    choice[0]=cars[i];
-        lcd.setCursor(1, 1);
-        lcd.print(choice[0]);
-        delay(750);
+        lcd.clear(); //clears the lcd screen
+        count++;//adds 1 to the count variable
+  	    choice[0]=cars[i];//stores the car choice as spot 1 in the choice array
+        lcd.setCursor(1, 1);//sets lcd position
+        lcd.print(choice[0]);//print users choice
+        delay(500);
         break;
       }
     }
@@ -53,23 +51,22 @@ void car()
 }
 void boat()
 {
-  while(count==1)
+  while(count==1)//while count variable is one print the boats array
     {
-      for(int i=0; i<5; i++)
+      for(int i=0; i<5; i++)//for loop to cycle through the items in the array
       {
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print(boats[i]);
         delay(750);
-        
-        if (digitalRead(buttonPin)==HIGH)
+        if (digitalRead(buttonPin)==HIGH)//checks to see if the button is pressed
         {
-          lcd.clear();
-          count++;
-  		  choice[1]=boats[i];       
-    	  lcd.setCursor(1, 1);
-    	  lcd.print(choice[1]);
-    	  delay(750);
+          lcd.clear();//clears the lcd screen
+          count++;//adds one to the count variable
+  		  choice[1]=boats[i];//stores the boats choice as the second spot in the choice array       
+    	  lcd.setCursor(1, 1);//sets lcd position
+    	  lcd.print(choice[1]);//prints users choice
+    	  delay(500);
           break;
         }
       }
@@ -79,21 +76,20 @@ void toy()
 {
    while(count==2)
      {
-       for(int i=0; i<5; i++)
+       for(int i=0; i<5; i++)//for loop to cycle through the items in the array
        {
          lcd.clear();
 		 lcd.setCursor(0,0);
          lcd.print(toys[i]);
          delay(750);
-         
-         if (digitalRead(buttonPin)==HIGH)
+         if (digitalRead(buttonPin)==HIGH)//checks to see if the button is pressed
          {
-           lcd.clear();
-           count++;
-  	 	   choice[2]=toys[i];
-           lcd.setCursor(1, 1);
-    	   lcd.print(choice[2]);
-    	   delay(750);
+           lcd.clear();//clears the lcd screen
+           count++;//adds one to the count variable
+  	 	   choice[2]=toys[i];//stores the toys choice as the third spot in the array
+           lcd.setCursor(1, 1);//sets the lcd position
+    	   lcd.print(choice[2]);//prints users choice
+    	   delay(500);
            break;
          }
        }
@@ -103,12 +99,49 @@ void end()
 {
   while(count==3)
     {
-      for(int i=0;i<3;i++)
+      for(int i=0;i<3;i++)//for loop to cycle through the items in the array
       {
         lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print(choice[i]);//prints the choice array
         lcd.setCursor(0,0);
-        lcd.print(choice[i]);
+        lcd.print("Your Choices Are");
         delay(500);
+         if (digitalRead(buttonPin)==HIGH)//checks if the button is pushed
+         {
+           count=-1;//sets count variable back to -1, starting the program again
+         }
       }
-    }
+    } 
+}
+void start()
+{
+  for (int i=0;i<=4;i++)//for loop to make welcome message scroll
+  {
+    lcd.clear();
+    lcd.setCursor(i,0);
+    lcd.print("Press Button");
+  	delay(100);
+  if (digitalRead(buttonPin)==HIGH)//checks to see if button is pressed
+  {
+    lcd.clear();
+    count=0;//sets count variable to zero, starting the program
+    delay(500);
+    break;
+  }
+  }
+  for (int i=5;i>=0;i--)//for loop to make second part of welcome message scroll opposite 
+  {
+    lcd.clear();
+    lcd.setCursor(i,1);
+    lcd.print("To Start");
+  	delay(100);
+  if (digitalRead(buttonPin)==HIGH)//check if button is pushed
+  {
+    lcd.clear();
+    count=0;//initializes program
+    delay(500);
+    break;
+  }
+  }
 }
--- lab_8_level_41.ino
+++ lab_8_level_41.ino
@@ -1,29 +1,29 @@
-// C++ code
-//
+// Kevin Bailey (200544764@student.georgianc.on.ca)
+// Wendnesday March 15 2023
+// Lab 8 Level 4 LCD yes or no
 
+//declare variables
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(12,11,5,4,3,2);
-
-//making a string array
-int buttonPin2 = 9;
-int buttonPin = 6;
+int buttonPin2 = 9;//button 2 is connected to pin 9 of the arduino
+int buttonPin = 6;//button 1 is connected to pin 6 of the arduino
+bool answers[]={};
 String questions[]={"Cars", "Boats", "Toys", "Planes", "Music"};
 int count=-1;
 int yes;
 int no;
 
-void setup()
+void setup()//The setup function runs once
 {
-Serial.begin(9600);
+Serial.begin(9600);//initialize the serial monitor
 lcd.begin (16,2); //Initialize the LCD.
-pinMode(buttonPin, INPUT);
-pinMode(buttonPin2, INPUT);
+pinMode(buttonPin, INPUT);//setup buttonPin as an INPUT pin
+pinMode(buttonPin2, INPUT);//setup buttonPin2 as an INPUT pin
 }
 	  
-void loop()
+void loop()//The loop function runs forever
 { 
-  start(); 
+  start(); //functions
   car();
   boat(); 
   toy();
@@ -33,7 +33,7 @@ void loop()
 }
 void car()
 {
-  while(count==0)
+  while(count==0)//while count variable is zero print the first question
   {
       lcd.clear();
 	  lcd.setCursor(0,0);
@@ -41,12 +41,12 @@ void car()
       lcd.setCursor(0,1);
       lcd.print("yes    no");
   	  delay(750);
-	  if (digitalRead(buttonPin)==HIGH)
+	  if (digitalRead(buttonPin)==HIGH)//checks if the button is pushed
 	  {
-        
+        answers[0]=true;//sets the first spot in the answers array to true or 1
         lcd.clear();
-        yes++;
-        count++;
+        yes++;//adds 1 to the yes variable
+        count++;//adds 1 to the count variable
         lcd.setCursor(0, 1);
         lcd.print("yes");
         delay(500);
@@ -54,19 +54,20 @@ void car()
       }
       else if (digitalRead(buttonPin2)==HIGH)
       {
+        answers[0]=false;
         lcd.clear();
-        no++;
-        count++;
+        no++;//adds 1 to the no variable
+        count++;//adds 1 to the count variable
         lcd.setCursor(7, 1);
         lcd.print("no");
         delay(500);
         break;
-      }          
+      } 
   }
 }
 void boat()
 {
-  while(count==1)
+  while(count==1)//while count variable is one print the second question
     {
         lcd.clear();
         lcd.setCursor(0,0);
@@ -76,9 +77,10 @@ void boat()
         delay(750);
         if (digitalRead(buttonPin)==HIGH)
         {
+          answers[1]=true;//sets the 2nd spot in the answers array to true or 1
           lcd.clear();
-          yes++;
-          count++;       
+          yes++;//adds 1 to the yes variable
+          count++;//adds 1 to the count variable       
     	  lcd.setCursor(0, 1);
     	  lcd.print("yes");
     	  delay(500);
@@ -86,20 +88,22 @@ void boat()
         }
     	else if (digitalRead(buttonPin2)==HIGH)
         {
+          answers[1]=false;
           lcd.clear();
-          no++;
-          count++;
+          no++;//adds 1 to the no variable
+          count++;//adds 1 to the count variable  
           lcd.setCursor(7, 1);
           lcd.print("no");
-          delay(500);
           break;
-        }     
+          delay(500);
+        }  
     }
 }
 void toy()
 {
-   while(count==2)
+   while(count==2)//while count variable is two print the third question
      {
+         answers[2]=true;//sets the 3rd spot in the answers array to true or 1
          lcd.clear();
 		 lcd.print("like " + questions[2] + "?");
     	 lcd.setCursor(0,1);
@@ -108,8 +112,8 @@ void toy()
          if (digitalRead(buttonPin)==HIGH)
          {
            lcd.clear();
-           count++;
-           yes++;
+           count++;//adds 1 to the count variable  
+           yes++;//adds 1 to the yes variable
            lcd.setCursor(0, 1);
     	   lcd.print("yes");
     	   delay(500);
@@ -117,9 +121,10 @@ void toy()
          }
      	 else if (digitalRead(buttonPin2)==HIGH)
          {
+           answers[2]=false;
            lcd.clear();
-           no++;
-           count++;
+           no++;//adds 1 to the no variable
+           count++;//adds 1 to the count variable  
            lcd.setCursor(7, 1);
            lcd.print("no");
            delay(500);
@@ -129,7 +134,7 @@ void toy()
 }
 void plane()
 {
-   while(count==3)
+   while(count==3)//while count variable is three print the fourth question
      {
          lcd.clear();
 		 lcd.print("like " + questions[3] + "?");
@@ -138,9 +143,10 @@ void plane()
          delay(750);
          if (digitalRead(buttonPin)==HIGH)
          {
+           answers[3]=true;//sets the 4th spot in the answers array to true or 1
            lcd.clear();
            count++;
-           yes++;
+           yes++;//adds 1 to the yes variable
            lcd.setCursor(0, 1);
     	   lcd.print("yes");
     	   delay(500);
@@ -148,9 +154,10 @@ void plane()
          }
      	 else if (digitalRead(buttonPin2)==HIGH)
          {
+           answers[3]=false;
            lcd.clear();
-           no++;
-           count++;
+           no++;//adds 1 to the no variable
+           count++;//adds 1 to the count variable
            lcd.setCursor(7, 1);
            lcd.print("no");
            delay(500);
@@ -160,7 +167,7 @@ void plane()
 }
 void music()
 {
-   while(count==4)
+   while(count==4)//while count variable is four print the fifth question
      {
          lcd.clear();
 		 lcd.print("like " + questions[4] + "?");
@@ -169,9 +176,10 @@ void music()
          delay(750);
          if (digitalRead(buttonPin)==HIGH)
          {
+           answers[4]=true;//sets the 5th spot in the answers array to true or 1
            lcd.clear();
-           count++;
-           yes++;
+           count++;//adds 1 to the count variable  
+           yes++;//adds 1 to the yes variable
            lcd.setCursor(0, 1);
     	   lcd.print("yes");
     	   delay(500);
@@ -179,9 +187,10 @@ void music()
          }
      	 else if (digitalRead(buttonPin2)==HIGH)
          {
+           answers[4]=false;
            lcd.clear();
-           no++;
-           count++;
+           no++;//adds 1 to the no variable
+           count++;//adds 1 to the count variable  
            lcd.setCursor(7, 1);
            lcd.print("no");
            delay(500);
@@ -191,9 +200,9 @@ void music()
 }
 void end()
 {
-  while(count==5)
+  while(count==5)//while count is five print the results
     {
-      for(int i=0;i<3;i++)
+      for(int i=0;i<5;i++)
       {
         lcd.clear();
         lcd.setCursor(0,0);
@@ -205,25 +214,28 @@ void end()
         lcd.setCursor(7,1);
         lcd.print("no");
         delay(500);
-         if (digitalRead(buttonPin)==HIGH || digitalRead(buttonPin2) ==HIGH)
+        Serial.println(answers[i]); 
+         if (digitalRead(buttonPin)==HIGH || digitalRead(buttonPin2) ==HIGH)//checks if either button has been pressed
          {
-           count=-1;
+           count=-1;//set count back to -1, start program again
          }
       }
     }  
 }
 void start()
 {
+  	yes=0;
+  	no=0;
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Left button=yes");
     lcd.setCursor(0,1);
     lcd.print("Right Button=no");
   	delay(250);
-  if (digitalRead(buttonPin)==HIGH || digitalRead(buttonPin2) ==HIGH)
+  if (digitalRead(buttonPin)==HIGH || digitalRead(buttonPin2) ==HIGH)//checks if either button is pressed
   {
     lcd.clear();
-    count=0;
+    count=0;//sets count variable to zero and initializes first question
     delay(500);
   }
 }
