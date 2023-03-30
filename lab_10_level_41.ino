536fd453ff95ccafba9ddffb1c96fc713c691e98
Bailz09 <122489154+Bailz09@users.noreply.github.com>
2023-03-30 10:54:07 -0500

    Add files via upload

--- lab_10_level_41.ino
+++ lab_10_level_41.ino
@@ -1,48 +1,51 @@
-#include <Wire.h>
+// Kevin Bailey (200544764@student.georgianc.on.ca)
+// Wednesday March 29 2023
+// Lab 10 Level 4 Dual Arduino Motor
 
-int buttonPin = 2;
-int buttonPin2 = 3;
-int value = 0;
+//declare variables
+#include <Wire.h>//Include Wire library for I2C communication
+int buttonPin = 2;// Define buttonPin as pin 2
+int buttonPin2 = 3;// Define buttonPin2 as pin 3
+int value = 0;//set value variable to zero
 
-void setup() {
-  pinMode(buttonPin, INPUT);
-  pinMode(buttonPin2, INPUT);
-
-  Wire.begin();
-  Serial.begin(9600);
+void setup()//the setup function runs once
+{
+  pinMode(buttonPin, INPUT);// Set buttonPin as input
+  pinMode(buttonPin2, INPUT);// Set buttonPin2 as input
+  Wire.begin();// Initialize I2C communication
+  Serial.begin(9600);//initialize the serial monitor
 }
 
-void loop() {
-  if (digitalRead(buttonPin) == HIGH) {
-    value++;
-    delay(300);
-    
-     int newvalue = value + 1;
-Serial.println(value);
-  Wire.beginTransmission(1); 
-  Wire.write(newvalue);      
-  Wire.endTransmission();    
-
-  delay(500);
+void loop()//the loop runs forever 
+{
+  if (digitalRead(buttonPin) == HIGH)//check if button 1 is pressed
+  {
+    value++;//increase the value variable by 1
+    delay(300);//delay
+    int newvalue = value + 1;//store value as value +1 so that a negative number is not sent through to output arduino
+	Serial.println(value);//print value to serial monitor
+    Wire.beginTransmission(1);//transmit the new value to Arduino(1) 
+    Wire.write(newvalue);      
+    Wire.endTransmission(); //end transmission   
+    delay(500);
   }
-  if (digitalRead(buttonPin2) == HIGH) {
-    value--;
-    delay(300);
-    
-    int newvalue = value + 1;
-Serial.println(value);
-  Wire.beginTransmission(1); 
-  Wire.write(newvalue);      
-  Wire.endTransmission();    
-
-  delay(500);
+  if (digitalRead(buttonPin2) == HIGH)//check if button 2 is pressed
+  {
+    value--;//decrease the value variable by 1
+    delay(300);//delay
+    int newvalue = value + 1;//store value as value +1 so that a negative number is not sent through to output arduino
+	Serial.println(value);//print value to serial monitor
+    Wire.beginTransmission(1); //transmit the new value to Arduino(1) 
+    Wire.write(newvalue);      
+    Wire.endTransmission();//end transmission     
+    delay(500);
   }
-
-  if (value < 0) {
+  if (value < 0)//resets the value variable if it goes outside the upper or lower limit
+  {
     value = 4;
-  } else if (value > 4) {
-    value = 0;
   }
-
- 
+   else if (value > 4) 
+   {
+     value = 0;
+   } 
 }
--- lab_10_level_42.ino
+++ lab_10_level_42.ino
@@ -1,114 +1,128 @@
-#include <Wire.h>
-#include <Servo.h>
+// Kevin Bailey (200544764@student.georgianc.on.ca)
+// Wednesday March 29 2023
+// Lab 10 Level 4 Dual Arduino Motor
 
+//declare variables
+#include <Wire.h>//Include Wire library for I2C communication
+#include <Servo.h>//Include Servo Library
 Servo myservo;
+int RGBRedpin = 9;//set pin 9 of arduino to the Red pin of the LED
+int RGBBluepin = 10;//set pin 10 of arduino to the Blue pin of the LED
+int RGBGreenpin = 11;//set pin 9 of arduino to the Green pin of the LED
+int value = 0;//set value variable to zero
+int count = 0;//set count variable to zero
 
-int RGBRedpin = 9;
-int RGBBluepin = 10;
-int RGBGreenpin = 11;
-int value = 0;
-int count = 0;
-
-void setup() {
-  myservo.attach(4);
-  pinMode(RGBRedpin, OUTPUT);
-  pinMode(RGBGreenpin, OUTPUT);
-  pinMode(RGBBluepin, OUTPUT);
-  myservo.write(0);
-
-  Wire.begin(1);          
+void setup()//the setup function runs once
+{
+  myservo.attach(4);//attach servo to pin 4 of arduino
+  pinMode(RGBRedpin, OUTPUT);//setup the Red pin of LED to an OUTPUT pin
+  pinMode(RGBGreenpin, OUTPUT);//setup the Green pin of LED to an OUTPUT pin
+  pinMode(RGBBluepin, OUTPUT);//setup the Blue pin of LED to an OUTPUT pin
+  myservo.write(0);//set initial servo position to zero
+  Wire.begin(1); //setup to receive from the input arduino         
   Wire.onReceive(receiveEvent);
-  Serial.begin(9600);
+  Serial.begin(9600);//initialize serial monitor
 }
 
-void loop() {
-  delay(500);
-  runProgram();
+void loop() //the loop function runs forever
+{
+  delay(500);//delay
+  runProgram();//run the runProgram function
 }
-
-void receiveEvent(int howMany)
-
-
+void receiveEvent(int howMany)//receive even function
+{
+  int newvalue = Wire.read();//read the value from input arduino    
+  value = newvalue-1;//set value back to original value
+  Serial.println(count);//print the count variable    
+}
+void runProgram()
+{
+  switch (value)//switch for the value variable 
   {
-  int newvalue = Wire.read();    
-  value = newvalue-1;
-  Serial.println(count);  
-  
-  
-}
-
-
-
-void runProgram() {
-
-  switch (value) {
-    case 0:
-      count = 1;
+    case 0://when value is zero
+    {
+      count = 1;//set count variable to 1
       break;
-    case 1:
+    }
+    case 1://and so on ...
+    {
       count = 2;
       break;
+    }
     case 2:
+    {
       count = 3;
       break;
+    }
     case 3:
+    {
       count = 4;
       break;
+    }
     case 4:
+    {
       count = 5;
       break;
+    }
     case 5:
+    {
       count = 6;
       break;
+    }
     case -1:
+    {
       count = 7;
       break;
+    }
   }
-
-  if (count == 1) {
+  if (count == 1)//if count variable is 1, run the red() function 
+  {
     red();
-  } else if (count == 2) {
-    green();
-  } else if (count == 3) {
-    blue();
-  } else if (count == 4) {
+  }
+   else if (count == 2)//and so on.. 
+   {
+     green();
+   }
+   else if (count == 3)
+   {
+     blue();
+   }
+  else if (count == 4)
+  {
     magenta();
-  } else if (count == 5) {
+  }
+  else if (count == 5)
+  {
     yellow();
-  } else if (count == 6) {
-    count = 0;
+  }
+  else if (count == 6)//when servo tries to go passed the upper limit
+  {
+    count = 0;//reset the variables back to original values
     value = 0;
-    magenta();
+    magenta();//run each function on the way back to starting position
     blue();
     green();
     red();
-    Serial.println("value is");
-    Serial.print(value);
-    Serial.println("count is");
-    Serial.print(count);
-  } else if (count == 7) {
-    count = 5;
+  }
+  else if (count == 7)//when servo tries to go below the lower limit 
+  {
+    count = 5;//set the variables to their max positions
     value = 4;
-    green();
+    green();//run each function on the way back to max position
     blue();
     magenta();
     yellow();
-    Serial.println("value is");
-    Serial.print(value);
-    Serial.println("count is");
-    Serial.print(count);
-    
   }
 }
 void red()
 { 
-  myservo.write(0);
+  myservo.write(0);//servo position when colour is red
   analogWrite(RGBRedpin, 255);
   analogWrite(RGBGreenpin, 0);
   analogWrite(RGBBluepin, 0);
   delay(500);
 }
-void green()
+void green()//and so on..
 {
   myservo.write(45);
   analogWrite(RGBRedpin, 0);
